bfefb2cb9076c7da5146ef0e1a6cc31bd2d1b5b7
Nobuyuki Inoue <spring555@gmail.com>
2019-04-14 01:04:23 +0900

    文字列コード指定機能追加

--- rsa_main.py
+++ rsa_main.py
@@ -12,54 +12,66 @@ def main():
     argc = len(argv)
 
     if argc < 2:
-        print("Usage: python %s [encrypt | decrypt | create_key] cleartext_file [encrypted_file]" %(argv[0]))
+        print("Usage: python %s [encrypt | decrypt | create_key] [平文ファイル | 暗号化ファイル] [公開鍵ファイル | 秘密鍵ファイル] [sjis | utf-8]" %(argv[0]))
         print("example1) -- create_key\n"
               "python rsa_main.py create_key\n\n"
-              "example2) -- encrypt\n"
-              "python rsa_main.py encrypt clearfile.txt encrypted.txt\n\n"
-              "example3) -- decrypt\n"
-              "python rsa_main.py decrypt encrypted.txt\n")
+              "example2-1) -- encrypt(Windows)\n"
+              "python rsa_main.py encrypt clearfile.txt rsa_public.key sjis\n\n"
+              "example2-2) -- encrypt(macOS/Linux)\n"
+              "python rsa_main.py encrypt clearfile.txt rsa_public.key utf-8\n\n"
+              "example3-1) -- decrypt(Windows)\n"
+              "python rsa_main.py decrypt encrypted.txt rsa_private.key sjis\n\n"
+              "example3-2) -- decrypt(macOS/Linux)\n"
+              "python rsa_main.py decrypt encrypted.txt rsa_private.key utf-8\n\n")
         exit(0)
 
     if argv[1] == "create_key":
         create_key()
 
     else:
-        if argc < 3:           
-            print("Usage: python %s [encrypt | decrypt | create_key] filename" %(argv[0]))
+        if argc < 4:           
+            print("Usage: python %s [encrypt | decrypt | create_key] [平文ファイル | 暗号化ファイル] [公開鍵ファイル | 秘密鍵ファイル] [sjis | utf-8]" %(argv[0]))
             exit(0)
 
         filename = argv[2]
         if not os.path.exists(filename):
             print("%s not found..." %filename)
             exit(0)
+        
+        keyfile = argv[3]
+        if not os.path.exists(keyfile):
+            print("%s not found..." %keyfile)
+            exit(0)
+
+        if argc == 5:
+            char_code = argv[4]
+        else:
+            char_code = "utf-8"
 
         if argv[1] == "encrypt":
-            encrypt(filename)
+            encrypt(keyfile, filename, char_code)
 
         elif argv[1] == "decrypt":
-            decrypt(filename)
-
-
-def check_targetfile_exists(filename):
-    if not os.path.exists(filename):
-        print("%s not found..." %filename)
-        exit(0)
+            decrypt(keyfile, filename, char_code)
 
 
 def create_key():
-    """公開鍵と秘密鍵を生成"""
-    public_key, private_key = rsa.generate_keys(101, 3259)
-
     print("Public key filename [rsa_public.key]:", end = '')
     public_key_filename = input()
     if public_key_filename == "":
         public_key_filename = "rsa_public.key"
 
+    check_exists(public_key_filename)
+
     print("Private key filename [rsa_private.key]:", end = '')
     private_key_filename = input()
     if private_key_filename == "":
         private_key_filename = "rsa_private.key"
+
+    check_exists(private_key_filename)
+
+    """公開鍵と秘密鍵を生成"""
+    public_key, private_key = rsa.generate_keys(101, 3259)
 
     with open(public_key_filename, mode='w') as f:
         f.writelines("%d,%d" %(public_key[0], public_key[1]))
@@ -70,35 +82,39 @@ def create_key():
     print("Create Keys done.")
 
 
-def read_keys():
-    public_key_filename  = "rsa_public.key"
-    private_key_filename = "rsa_private.key"
+def check_exists(filename):
+    if os.path.exists(filename):
+        print("%s is exists." %filename)
+        print("overwrite ? (Y/N):", end=None)
 
-    """公開鍵ファイルの読み込み"""
-    f1 = open(public_key_filename, "r")
+        firstKey = ''
+        while (firstKey != 'Y' and firstKey != 'N'):
+            print("\n%s is exists. overwrite? (Y/N)" %filename, end = "")
+            keyRet = ord(getch())
+            firstKey = chr(keyRet).upper()
+
+        print()
+        if (firstKey == 'N'):
+            print("quit....")
+            exit(0)
+
+
+def read_key(keyfilename):
+    """鍵ファイルの読み込み"""
+    f1 = open(keyfilename, "r")
     lines = f1.readlines()
 
     """最初の１行から２つの値を読み込む"""
     flds = lines[0].split(",")
-    public_key = (int(flds[0]), int(flds[1]))
-
-    """秘密鍵ファイルの読み込み"""
-    f2 = open(private_key_filename, "r")
-    lines = f2.readlines()
-
-    """最初の１行から２つの値を読み込む"""
-    flds = lines[0].split(",")
-    private_key = (int(flds[0]), int(flds[1]))
-
-    return public_key, private_key
+    return (int(flds[0]), int(flds[1]))
 
 
-def encrypt(cleartext_fname):
+def encrypt(keyfile, cleartext_fname, char_code):
     """鍵ファイルから公開鍵を読み込む"""
-    public_key, _ = read_keys()
+    public_key = read_key(keyfile)
 
     """平文ファイルを読み込む"""
-    f = open(cleartext_fname, "rt", encoding="utf-8")
+    f = open(cleartext_fname, "rt", encoding=char_code)
     lines = f.readlines()
 
     plain_text = ""
@@ -107,16 +123,15 @@ def encrypt(cleartext_fname):
 
     """暗号化および結果の出力"""
     encrypted_text = rsa.encrypt(plain_text, public_key)
-#    print(rsa.sanitize(encrypted_text))
     print(encrypted_text)
 
 
-def decrypt(filename):
+def decrypt(keyfile, filename, char_code):
     """鍵ファイルから秘密鍵を読み込む"""
-    _, private_key = read_keys()
+    private_key = read_key(keyfile)
 
     """暗号文ファイルを読み込む"""
-    f = open(filename, "r")
+    f = open(filename, "r", encoding=char_code)
     lines = f.readlines()
 
     encrypted_text = ""
